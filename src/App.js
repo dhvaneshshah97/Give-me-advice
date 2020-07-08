a42dd6b982c2a6d2a1b8abf0e10ea858c73dae2d
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-07-07 18:03:02 -0700

    Fetch data from API and add some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css'; 
+import './App.css';
 import axios from 'axios';
 
 export default class App extends Component {
@@ -10,14 +10,32 @@ export default class App extends Component {
         }
     }
 
+    fetchAdvice = async () => {
+        const response = await axios.get('https://api.adviceslip.com/advice');
+        if (response.data) {
+            const { advice } = response.data.slip;
+            this.setState({
+                advice,
+            });
+        } else {
+            console.log(response.error);
+        }
+    }
+
     componentDidMount() {
-        console.log("hello");
+        this.fetchAdvice();
     }
 
     render() {
+        const { advice } = this.state;
         return (
-            <div>
-                <h1>APP</h1>
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                    <button className="button" onClick={this.fetchAdvice}>
+                        <span>Give me advice!</span>
+                    </button>
+                </div>
             </div>
         );
     }
